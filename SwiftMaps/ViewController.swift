561b79dc47569a6f3169488ebb18443a1e0761df
Philip Schneider <pschneider@hausgross.de>
2016-07-09 21:11:46 +0200

    CLLocation Delegate
    Zeige UserLocation an

--- SwiftMaps/ViewController.swift
+++ SwiftMaps/ViewController.swift
@@ -7,19 +7,81 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            self.locationManager = CLLocationManager()
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.startUpdatingLocation()
+        }
+        
+//        // To complete the authorization process for enabling location services, add the following lines of code
+        let status = CLLocationManager.authorizationStatus()
+        if status == .NotDetermined || status == .Denied || status == .AuthorizedWhenInUse {
+            // present an alert indicating location authorization required
+            // and offer to take the user to Settings for the app via
+            // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
+//            self.locationManager.requestAlwaysAuthorization()
+            self.locationManager.requestWhenInUseAuthorization()
+        }
+        self.locationManager.startUpdatingLocation()
+        self.locationManager.startUpdatingHeading()
+        
+     
+        self.mapView = MKMapView(frame: self.view.frame)
+        self.mapView.autoresizingMask = self.view.autoresizingMask
+        self.mapView.delegate = self
+        
+        self.mapView.showsScale = true
+        self.mapView.showsCompass = true;
+        self.mapView.showsTraffic = true;
+        self.mapView.showsBuildings = true;
+        self.mapView.showsPointsOfInterest = true;
+        
+        self.mapView.mapType = MKMapType(rawValue: 0)!
+        self.mapView.showsUserLocation = true;
+        self.mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+        
+        self.view.addSubview(self.mapView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        locationManager.startUpdatingHeading()
+        locationManager.startUpdatingLocation()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        locationManager.stopUpdatingHeading()
+        locationManager.stopUpdatingLocation()
+    }
+    
+    
+    // CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+    }
 
 }
 
