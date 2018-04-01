d18140ff924096d4537531587a999b8a958ef0b3
Philip Schneider <pschneider@hausgross.de>
2018-04-01 12:28:04 +0200

    Feature: Thunderforest Pioneer

--- SwiftMaps/AppDelegate.swift
+++ SwiftMaps/AppDelegate.swift
@@ -791,6 +791,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             sortOrder=NSNumber(value:sortOrder.intValue + 1)
         }
 
+        importName = "Thunderforest-Pioneer+CoreDataTileOverlay"
+        if (UserDefaults.standard.bool(forKey: importName) == false)
+        {
+            tile = NSEntityDescription.insertNewObject(forEntityName: "Tile", into: managedObjectContext)
+            tile.setValue("Thunderforest Pioneer", forKeyPath: "name")
+            tile.setValue("CoreDataTileOverlay", forKeyPath: "classFileName")
+            tile.setValue("tile.thunderforest.com/pioneer/{z}/{x}/{y}", forKeyPath: "url")
+            tile.setValue(NSNumber.init(value:true), forKeyPath: "useHttps")
+            tile.setValue(NSNumber.init(value:true), forKeyPath: "useLoadbalancing")
+            tile.setValue(sortOrder, forKeyPath: "sortOrder")
+
+            UserDefaults.standard.register(defaults: [importName : true])
+            UserDefaults.standard.set(true, forKey: importName)
+            UserDefaults.standard.synchronize()
+
+            sortOrder=NSNumber(value:sortOrder.intValue + 1)
+        }
+
         do {
             try managedObjectContext.save()
         } catch {
