2d793db7558fe7972e9a22be113ba6c05b49c892
Philip Schneider <pschneider@hausgross.de>
2018-04-03 15:03:14 +0200

    Feature: Compressed Air

--- SwiftMaps/AppDelegate.swift
+++ SwiftMaps/AppDelegate.swift
@@ -1169,7 +1169,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             sortOrder=NSNumber(value:sortOrder.intValue + 1)
         }
         
-        
+        importName = "Amenity-CompressedAid-Imported"
+        if (UserDefaults.standard.bool(forKey: importName) == false)
+        {
+            poi = NSEntityDescription.insertNewObject(forEntityName: "Poi", into: managedObjectContext)
+            poi.setValue("amenity", forKeyPath: "category")
+            poi.setValue("compressed_air", forKeyPath: "type")
+            poi.setValue(sortOrder, forKeyPath: "sortOrder")
+            
+            UserDefaults.standard.register(defaults: [importName : true])
+            UserDefaults.standard.set(true, forKey: importName)
+            UserDefaults.standard.synchronize()
+            
+            sortOrder=NSNumber(value:sortOrder.intValue + 1)
+        }
         
         do {
             try managedObjectContext.save()
